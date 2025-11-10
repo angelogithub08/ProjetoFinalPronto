a2aa825b416d37f55409853e8292e0baa2b18c6e
angelogithub08 <54257022+angelogithub08@users.noreply.github.com>
2025-11-09 21:42:09 -0300

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -17,27 +17,15 @@ from sqlmodel import SQLModel
 # Importar todos os models para que sejam detectados pelo Alembic
 from models import User, TransactionType, Transaction
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
 # Configurar URL do banco de dados
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 target_metadata = SQLModel.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
